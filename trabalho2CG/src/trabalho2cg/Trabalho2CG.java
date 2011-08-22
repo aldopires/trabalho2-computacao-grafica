a85e1e724889f5fb6c5339f03785e5c42a441594
hfaccioni <hfaccioni@8ea7cc74-bea4-f281-1d07-6ea067bc756f>
2011-08-22 19:01:40 +0000

    criei a estrutura MVC ai pro trabalho... soh isso.. por enquanto.

---
+++ trabalho2CG/src/trabalho2cg/Trabalho2CG.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho2cg;
+
+/**
+ *
+ * @author Heitor
+ */
+public class Trabalho2CG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
