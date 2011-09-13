ef7cf9b0687596cee2e1c04b80336d97075c3987
tytu.victor@gmail.com <tytu.victor@gmail.com@8ea7cc74-bea4-f281-1d07-6ea067bc756f>
2011-09-13 03:41:49 +0000

    as coisa ae mano

--- trunk/trabalho2CG/src/modelo/Aresta.java
+++ trunk/trabalho2CG/src/modelo/Aresta.java
@@ -12,5 +12,23 @@ public class Aresta {
     
     private Vertice v1= new Vertice();
     private Vertice v2= new Vertice();
+
+    public Vertice getV1() {
+        return v1;
+    }
+
+    public void setV1(Vertice v1) {
+        this.v1 = v1;
+    }
+
+    public Vertice getV2() {
+        return v2;
+    }
+
+    public void setV2(Vertice v2) {
+        this.v2 = v2;
+    }
+    
+    
     
 }
--- trunk/trabalho2CG/src/modelo/Face.java
+++ trunk/trabalho2CG/src/modelo/Face.java
@@ -11,7 +11,7 @@ import java.util.List;
  *
  * @author TyTu
  */
-class Face {
+public class Face {
     
     private List<Aresta> arestas= new ArrayList<Aresta>();
     
