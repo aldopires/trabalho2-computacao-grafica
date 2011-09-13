9262554564275829a4effafa67587281e83081fe
tytu.victor@gmail.com <tytu.victor@gmail.com@8ea7cc74-bea4-f281-1d07-6ea067bc756f>
2011-09-13 06:20:24 +0000

    objeto com centro

--- trabalho2CG/src/modelo/Objeto.java
+++ trabalho2CG/src/modelo/Objeto.java
@@ -13,6 +13,7 @@ import java.util.List;
  */
 public class Objeto {
     private List<Face> faces;
+    private Vertice centro;
 
     public Objeto() {
         faces =  new ArrayList<Face>();
@@ -21,11 +22,53 @@ public class Objeto {
     public boolean addFace(Face f){
         try{
             faces.add(f);
+            atualizaCentro();
             return true;
         }catch(Exception e){
             return false;
         }
     }
+
+    public Vertice getCentro() {
+        return centro;
+    }
+    
+    
+    
+    public void atualizaCentro(){
+        float minx=9999,maxx=-9999,miny=9999,maxy=-9999,minz=9999,maxz=-9999;
+        for(int i=0;i<faces.size();i++){
+            for(int j=0;j<faces.get(i).getArestas().size();j++){
+                if(minx>faces.get(i).getArestas().get(j).getV1().getX())                    
+                    minx=faces.get(i).getArestas().get(j).getV1().getX();
+                if(minx>faces.get(i).getArestas().get(j).getV2().getX())
+                    minx=faces.get(i).getArestas().get(j).getV2().getX();
+                if(miny>faces.get(i).getArestas().get(j).getV2().getY())
+                    miny=faces.get(i).getArestas().get(j).getV1().getY();
+                if(miny>faces.get(i).getArestas().get(j).getV2().getY())
+                    miny=faces.get(i).getArestas().get(j).getV2().getY();
+                if(minz>faces.get(i).getArestas().get(j).getV2().getZ())
+                    minz=faces.get(i).getArestas().get(j).getV1().getZ();
+                if(minz>faces.get(i).getArestas().get(j).getV2().getZ())
+                    minz=faces.get(i).getArestas().get(j).getV2().getZ();
+                if(maxx<faces.get(i).getArestas().get(j).getV1().getX())                    
+                    maxx=faces.get(i).getArestas().get(j).getV1().getX();
+                if(maxx<faces.get(i).getArestas().get(j).getV2().getX())
+                    maxx=faces.get(i).getArestas().get(j).getV2().getX();
+                if(maxy<faces.get(i).getArestas().get(j).getV2().getY())
+                    maxy=faces.get(i).getArestas().get(j).getV1().getY();
+                if(maxy<faces.get(i).getArestas().get(j).getV2().getY())
+                    maxy=faces.get(i).getArestas().get(j).getV2().getY();
+                if(maxz<faces.get(i).getArestas().get(j).getV2().getZ())
+                    maxz=faces.get(i).getArestas().get(j).getV1().getZ();
+                if(maxz<faces.get(i).getArestas().get(j).getV2().getZ())
+                    maxz=faces.get(i).getArestas().get(j).getV2().getZ();
+            }
+        }
+        Vertice v = new Vertice((maxx-minx)/2, (maxy-miny)/2, (maxz-minz)/2);
+        centro=v;
+    }
+
     public int getSize(){
         return this.faces.size();
     }
@@ -33,6 +76,7 @@ public class Objeto {
     public List<Face> getFaces(){
         return this.faces;
     }
+
     
     
 }
