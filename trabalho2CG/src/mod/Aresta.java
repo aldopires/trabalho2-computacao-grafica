f45d5c6b85462f8582427cd0649598fe8352e555
tytu.victor@gmail.com <tytu.victor@gmail.com@8ea7cc74-bea4-f281-1d07-6ea067bc756f>
2011-09-16 04:45:01 +0000

    extrusao agora pra figuras sem faces ou sem arestas
    é pra ta funfando

--- trabalho2CG/src/mod/Aresta.java
+++ trabalho2CG/src/mod/Aresta.java
@@ -26,6 +26,32 @@ public class Aresta {
     public Aresta clone(){
         return (new Aresta(v1.clone(), v2.clone()));
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aresta other = (Aresta) obj;
+        if (this.v1 != other.v1 && (this.v1 == null || !this.v1.equals(other.v1))) {
+            return false;
+        }
+        if (this.v2 != other.v2 && (this.v2 == null || !this.v2.equals(other.v2))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (this.v1 != null ? this.v1.hashCode() : 0);
+        hash = 79 * hash + (this.v2 != null ? this.v2.hashCode() : 0);
+        return hash;
+    }
     
     
 
