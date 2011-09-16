fb5bd0dfddf7aeffba952ef3628e532cdc8b9903
hfaccioni <hfaccioni@8ea7cc74-bea4-f281-1d07-6ea067bc756f>
2011-09-16 11:49:09 +0000

    Telas criadas da seguinte maneira: 1 - uma tela principal com as 4 visoes separadas. 2 - uma tela para cada visão, acessada clicando no icone no cando inferior esquerdo - para a exibição de qualquer tipo podemos passar a lista de pontos, como parametro, que serão desenhados no novo Frame e ao clicar na tela o desenho é feitto.

---
+++ trunk/trabalho2CG/src/modelo/Painel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Heitor
+ */
+public class Painel extends JPanel {
+    
+    
+}
